d01e9a612910ed08447ee2f3e343b06ee45a2ada
Frank-003 <oyiengofrankline49@gmail.com>
2024-09-17 13:09:20 +0000

    task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,6 @@
-// Function to display a message
 function displayMessage(message) {
   console.log(message);  // Prints the message to STDOUT
-}
+};
 
 // Export the function (if needed)
 module.exports = displayMessage;
