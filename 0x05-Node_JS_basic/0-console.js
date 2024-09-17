9132000daf6930c8b2c06a49fe4f398093402802
Frank-003 <oyiengofrankline49@gmail.com>
2024-09-17 12:55:35 +0000

    task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+// Function to display a message
+function displayMessage(message) {
+  console.log(message);  // Prints the message to STDOUT
+}
+
+// Export the function (if needed)
+module.exports = displayMessage;
+
