40bcacce22b823da022d91286abb7bdabeb2b8fb
Frank-003 <oyiengofrankline49@gmail.com>
2024-11-29 08:59:06 +0000

    task 0

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,20 +1,21 @@
 const http = require('http');
 
-// Create the HTTP server
-const app = http.createServer((req, res) => {
-  // Set the response header to indicate plain text content
-  res.setHeader('Content-Type', 'text/plain');
-  
-  // Send the response body
-  res.end('Hello Holberton School!');
-});
-
-// Listen on port 1245
 const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+const HOST = 'localhost';
+const app = http.createServer();
+
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-// Export the app for external use (e.g., in tests)
-module.exports = app;
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
+module.exports = app;
